b2304e9fc323c1699789c8570eaf3555f161e3e0
Pratik Unde <pratikunde9@gmail.com>
2025-05-25 23:12:53 +0530

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,55 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Photographer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'PRATIK';
+
+export const experiences = [
+    {
+        'company': 'ThinkQuotient Software Pvt Ltd',
+        'role': 'Software Developer Intern',
+        'duration': 'April 2023 - Sep 2023',
+        'logo': 'https://www.thinkquotient.com/assets/images/envelop.png',
+        'points': [
+            'Developed small-scale web applications using HTML, CSS, JavaScript, and basic React.js.',
+            'Assisted in building RESTful APIs and integrated frontend with backend services.',
+            'Collaborated with senior developers in debugging and optimizing code.',
+            'Worked on assignments related to data structures, algorithms, and problem-solving.',
+            'Gained exposure to Git, GitHub, and Agile development methodologies.'
+        ],
+        // 'url': 'https://inncircles.com/#home',
+    }
+    ,
+    {
+        'company': 'Jisa Softech Pvt Ltd',
+        'role': 'Software Developer',
+        'duration': 'Sep 2023 - Current',
+        'logo': 'https://www.jisasoftech.com/wp-content/uploads/2021/10/logo_test.png',
+        'points': [
+            'Developed secure APIs and backend services using Java, Spring Boot, and MongoDB.',
+            'Integrated cloud-based key management (Azure, AWS, GCP) with Salesforce and other platforms.',
+            'Built modules for data masking, JWT authentication, and certificate management.',
+            'Worked on integrating HSM-protected key signing and encryption/decryption services.',
+            'Collaborated across teams to deliver enterprise-grade secure solutions and improved system reliability.'
+        ],
+        // 'url': 'https://www.att.com/',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
